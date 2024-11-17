b24a2c7c3f59deaaee90e6b9a946faa61b624f6e
KONICCO <nico678514@gmail.com>
2024-11-17 22:07:56 +0700

    migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Events\QueryExecuted;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        DB::listen(function (QueryExecuted $query){
+            Log::info($query->sql);
+        });
     }
 }
